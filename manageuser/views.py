4a8052e9f4580979e3426348a455488da5f2ca7c
Vinicius <vinivice@gmail.com>
2017-07-02 23:26:19 -0700

    basic login working

--- manageuser/views.py
+++ manageuser/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
 
 def index(request):
 	if request.user.is_authenticated():
@@ -15,5 +17,16 @@ def register(request):
 #		return HttpResponse("<h2>FUNCIONA REGISTER NOT NONE</h2>")
 
 def login(request):
-	print(request.POST)
-	return render(request, 'manageuser/login.html')
+	if request.method == 'POST':
+		print("test post")
+		print(request.POST)
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			auth_login(request, user)
+			return HttpResponse("<h2>USER OK</h2>")
+		else:
+			return render(request, 'manageuser/login.html', request.POST)
+	else:
+		return render(request, 'manageuser/login.html')
