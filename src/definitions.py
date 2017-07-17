be1098c53651d4d66e806bef441284f36eb38981
CarlosAd <carlos.adpinto@outlook.com>
2017-07-17 18:16:59 -0300

    Creation of directory for each type of analysis aborted

--- src/definitions.py
+++ src/definitions.py
@@ -89,11 +89,10 @@ def generate_reports(full_path, file_path):
 	print("SAVE FILE")
 
 def generate_static_reports(full_path, file_path):
-        call(["mkdir", "-p", full_path + "/reports/static"])
         static_analysis_string = check_output(["peframe", "--json", file_path]).decode("utf-8")
         strings_report_string = check_output(["peframe", "--strings", file_path]).decode("utf-8")
         final_string = "{\n\"analysis\":" + static_analysis_string[:-1] + ",\n\"strings\":" + strings_report_string + "}"
-        with open(full_path + "/reports/static/report.json", "w+") as outputfile:
+        with open(full_path + "/reports/static_analysis.json", "w+") as outputfile:
                 outputfile.write(final_string)
 
 def generate_dynamic_reports(full_path, file_path):
